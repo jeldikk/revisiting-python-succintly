609fef6852df8821fb20452da5a12fe7ee099d9e
jeldikk <jeldi.kamal2013@gmail.com>
2024-12-09 11:53:35 +0530

    setup environment, folder structure and little learning about variables

---
+++ src/01_variable_string/variables.py
@@ -0,0 +1,31 @@
+## these are comments in python code
+
+
+## variables are named storage locations. also described as name-value pairs
+vegetables = 'asparagus'
+
+# we can change the value of variable by reassigning it. keep name of variable something meaning, so it will assist you later time when you revisit the code
+vegetables = 'onion'
+
+# variable names are case sensitive
+'''
+By convention, variables are usually named with all lower 
+case letters, but this is by no means a requirement. While variable names can contain numbers in the 
+body of the name, they must always start with a letter. Another rule to remember is that you cannot use 
+a hyphen (-), plus sign (+), or other various symbols in variable names. You can however use the 
+underscore (_) character
+'''
+Vegetable = 'Onion'
+vegetable = 'onion'
+
+# with number in between
+last3Letters = 'XYZ'
+
+# multi word variable name in snake case
+last_three_letters = 'XYZ'
+
+# multi word variable name in came case
+lastThreeLetters = 'XYZ'
+
+
+## Strings
